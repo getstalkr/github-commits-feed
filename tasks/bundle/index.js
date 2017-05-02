1f06fab798d954761b86f80353700f13be9ba662
Matheus Brasil <matheus.brasil10@gmail.com>
2017-05-02 00:23:59 -0300

    chore(bundling): setup web pack + nodemon

---
+++ tasks/bundle/index.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack')
+const nodemon = require('nodemon')
+const path = require('path')
+const chalk = require('chalk')
+const ora = require('ora')
+const argv = require('yargs').argv
+const config = require('./webpack.config.js')
+
+const log = console.log
+const watchFlag = argv.watch
+
+if (watchFlag) {
+  let runner
+  const run = () => nodemon({
+    execMap: { js: 'node' },
+    script: path.join(__dirname, '../build/index.js'),
+    ignore: ['*'],
+    watch: ['nothing/'],
+    ext: 'noop'
+  })
+
+  const starter = ora('Building...').start()
+  webpack(config).watch(100, () => {
+    if (runner) {
+      log(chalk.gray('Restarting...'))
+      nodemon.restart()
+    } else {
+      starter.succeed('Built!')
+      runner = run()
+    }
+  })
+
+  process.once('SIGINT', () => {
+    log()
+    log(chalk.yellow('👋  Goodbye'))
+    if (runner) runner.once('exit', process.exit)
+    else process.exit()
+  })
+} else {
+
+  const starter = ora('Building...').start()
+  webpack(config).run(() => {
+    starter.succeed('Built!')
+  })
+
+}
---
+++ tasks/bundle/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const webpack = require('webpack')
+const fs = require('fs')
+
+const projectRoot = path.resolve()
+
+const nodeModules = {}
+
+fs.readdirSync('node_modules')
+  .filter(x => ['.bin'].indexOf(x) === -1)
+  .forEach((mod) => {
+    nodeModules[mod] = `commonjs ${mod}`
+  })
+
+
+const config = {
+  target: 'node',
+  entry: [
+    path.resolve(projectRoot, 'src/index.ts')
+  ],
+  output: {
+    libraryTarget: 'commonjs2',
+    filename: 'index.js',
+    path: projectRoot
+  },
+
+  recordsPath: path.join(projectRoot, 'build/_records'),
+  resolve: {
+    modules: [path.resolve(projectRoot, 'node_modules')],
+    extensions: [".ts", ".js", ".json"]
+  },
+  externals: nodeModules,
+  devtool: '#eval-source-map',
+  module: {
+    rules: [
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['source-map-loader']
+      },
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      {
+        test: /\.ts?$/,
+        loader: "awesome-typescript-loader"
+      },
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: 'require("source-map-support").install();',
+      raw: true,
+      entryOnly: false
+    }),
+    new webpack.NamedModulesPlugin()
+  ]
+}
+
+module.exports = config
